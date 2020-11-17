49628d482988419da71e3e41012bde3ff8b39dd1
HanaPos <h.hedbavna@gmail.com>
2020-11-17 21:07:38 +0100

    Rendering cards added

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,25 +1,52 @@
 export default class Card {
-  constructor() {}
+  constructor(id, name, img = '', text = '') {
+    this._id = id;
+    this._name = name;
+    this._img = img;
+    this._text = text;
+  }
+
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get img() {
+    return this._img;
+  }
+  get text() {
+    return this._text;
+  }
 
   createClassElement(el, classNames) {
     const element = document.createElement(el);
     element.classList.add(...classNames);
     return element;
   }
-  renderCard() {
+
+  createCard() {
     const card = this.createClassElement('div', ['card']);
+    card.setAttribute('id', this._id);
     const cardInner = this.createClassElement('div', ['card-inner']);
     const cardFront = this.createClassElement('div', ['card-front']);
     const cardBack = this.createClassElement('div', ['card-back']);
-    const img = document.createElement('img');
-    img.setAttribute('src', '../images/error.jpg');
-    const p = document.createElement('p');
-    p.append('❤');
-    cardFront.append(p);
-    cardBack.append(img);
+    const frontText = document.createElement('p');
+    frontText.append('❤');
+    cardFront.append(frontText);
+    if (this._img) {
+      const img = document.createElement('img');
+      img.src = this._img;
+      cardBack.append(img);
+    }
+    if (this._text) {
+      const cardText = document.createElement('p');
+      cardText.append(this._text);
+      cardBack.append(cardText);
+    }
     cardInner.append(cardFront, cardBack);
     card.append(cardInner);
-
+    console.log(card);
     return card;
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,27 @@ import Card from './Card';
 import Countries from './Countries';
 
 //https://restcountries.eu/rest/v2/region/europe
+const gameContainer = document.querySelector('.game-container');
 
 const countriesApiService = new Countries(
   'https://restcountries.eu/rest/v2/region/'
 );
-countriesApiService.getData('europe');
-const gameContainer = document.querySelector('.game-container');
-for (let i = 0; i < 56; i++) {
-  gameContainer.append(new Card().renderCard());
-}
+(async function () {
+  const data = await countriesApiService.getData('europe');
+  //console.log(data);
+
+  for (let i = 0; i < 28; i++) {
+    console.log(data[i]);
+    gameContainer.append(new Card(i, data[i].name, data[i].flag).createCard());
+    gameContainer.append(
+      new Card(i * 2, data[i].name, undefined, data[i].name).createCard()
+    );
+  }
+})();
+
+const flip = function (event) {
+  const card = event.target.closest('.card');
+  card.classList.toggle('flip');
+};
+
+gameContainer.addEventListener('click', flip);
